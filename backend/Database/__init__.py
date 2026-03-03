7db8621fe9b94807b1df2c2f6860feb84f9c8fdf
Geoff-Robin <jeffrobin132004@gmail.com>
2026-03-03 11:11:47 +0530

    Made few changes to dal

--- backend/Database/__init__.py
+++ backend/Database/__init__.py
@@ -1,6 +1,5 @@
 from Database.config import get_engine
-from Database.models import Base, User, Student, Exam, Question, Answers
-from Database.user_dal import get_user_dal
+from Database.models import Base, Student, Exam, Question, Answers
 from Database.student_dal import get_student_dal
 from Database.questions_dal import get_question_dal
 from Database.exam_dal import get_exam_dal
@@ -8,13 +7,11 @@ from Database.answers_dal import get_answers_dal
 
 __all__ = [
     "get_engine",
-    "get_user_dal",
     "get_student_dal",
     "get_question_dal",
     "get_exam_dal",
     "get_answers_dal",
     "Base",
-    "User",
     "Student",
     "Exam",
     "Question",
