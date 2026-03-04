914eff60ac5977fe5260ad194aaca148fb5aa312
Geoff-Robin <jeffrobin132004@gmail.com>
2026-03-04 12:36:38 +0530

    Added KnowledgeBase Table

--- backend/Database/__init__.py
+++ backend/Database/__init__.py
@@ -1,9 +1,10 @@
 from Database.config import get_engine
-from Database.models import Base, Student, Exam, Question, Answers
+from Database.models import Base, Student, Exam, Question, Answers, KnowledgeBase
 from Database.student_dal import get_student_dal
 from Database.questions_dal import get_question_dal
 from Database.exam_dal import get_exam_dal
 from Database.answers_dal import get_answers_dal
+from Database.knowledge_base_dal import get_knowledge_base_dal
 
 __all__ = [
     "get_engine",
@@ -11,9 +12,11 @@ __all__ = [
     "get_question_dal",
     "get_exam_dal",
     "get_answers_dal",
+    "get_knowledge_base_dal",
     "Base",
     "Student",
     "Exam",
     "Question",
-    "Answers"
+    "Answers",
+    "KnowledgeBase",
 ]
