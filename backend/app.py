f6e8126a9cd6b9be9b1ab6ec6e92d9e84899f7c5
Geoff-Robin <jeffrobin132004@gmail.com>
2026-03-05 09:46:35 +0530

    Added grading faststream router and removed allow_origins list

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,11 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
-from config import origins
 from routes import exam_router
 from fastapi.middleware.cors import CORSMiddleware
 from Database.config import get_engine
 from Database.models import Base
+from Grading import grading_task_router
 
 load_dotenv()
 
@@ -28,6 +28,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan, debug=True)
 
 app.include_router(exam_router, prefix="/api/exam")
+app.include_router(grading_task_router)
 
 @app.post("/api")
 async def root():
@@ -35,7 +36,6 @@ async def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
